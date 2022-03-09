54a91acd0053ee32ee3b17bdb8cac8d143650096
dipanc1 <dipanchhabra@gmail.com>
2022-03-09 17:10:10 +0530

    first commit

---
+++ src/components/Box.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Birthday from './Birthday';
+import './box.css'
+
+const Box = () => {
+    const [card, setCard] = React.useState(false);
+    const handleClick = () => {
+        setCard(true);
+        console.log('card');
+    }
+    return (
+        !card ? (
+            <div className="birthday-gift">
+                <div className="gift">
+                    <input id='click' type='checkbox' />
+                    <label className='click' for='click' onClick={handleClick}></label>
+                    <div className="wishes">Happy Birthday!</div>
+                    <div className="sparkles">
+                        <div className="spark1"></div>
+                        <div className="spark2"></div>
+                        <div className="spark3"></div>
+                        <div className="spark4"></div>
+                        <div className="spark5"></div>
+                        <div className="spark6"></div>
+                    </div>
+                </div>
+            </div>
+        ) : (<Birthday />)
+    )
+}
+
+export default Box
---
+++ src/components/Countdown.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { useTimer } from 'react-timer-hook';
+import Box from './Box';
+import './countdown.css'
+
+const MyTimer = ({ expiryTimestamp }) => {
+    const {
+        seconds,
+        minutes,
+        hours,
+        days,
+        isRunning,
+    } = useTimer({ expiryTimestamp, onExpire: () => console.warn('onExpire called') });
+
+    const hourTime = hours < 10 ? `0${hours}` : `${hours}`;
+    const dayTime = days < 10 ? `0${days}` : `${days}`;
+    const secondTime = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    const minuteTime = minutes < 10 ? `0${minutes}` : `${minutes}`;
+
+    return (
+        isRunning ?
+            (<div className="container">
+                <h1 id="headline" > Time Till My Baby's Birthday </h1>
+                <div id="countdown" >
+                    <ul>
+                        <li><span id="days">{dayTime}</span>days</li>
+                        <li><span id="hours">{hourTime}</span>Hours</li>
+                        <li><span id="minutes">{minuteTime}</span>Minutes</li>
+                        <li><span id="seconds">{secondTime}</span>Seconds</li>
+                    </ul>
+                </div>
+            </div>) :
+            (<Box />)
+    );
+}
+const Countdown = () => {
+    const time = new Date();
+    time.setSeconds(time.getSeconds() + 3); // have to change
+
+    return (
+        <div>
+            <MyTimer expiryTimestamp={time} />
+        </div>
+    );
+}
+
+export default Countdown
