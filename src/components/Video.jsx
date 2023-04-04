068f596ad0e6ef085f3d11bfc5d7d41a8d336e6a
dipanc1 <dipanchhabra@gmail.com>
2023-04-04 19:19:06 +0530

    updated video

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,11 +1,12 @@
 import './video.css'
+import video from './images/video.mp4'
 
 const Video = () => {
   return (
     <div className='video'>
-        <h1 className='clickhere9'>ALLES GUTE ZUM GEBURTSTAG, MEINE PUPPE</h1>
-        <video src="https://res.cloudinary.com/dipanc1/video/upload/v1648792386/birthday_web_app/video_y82zhx.mp4" controls autoplay loop className='videoPlayer'></video>
-        <h1 className='clickhere8'>Mein liebes Baby</h1>
+      <h1 className='clickhere9'>ALLES GUTE ZUM GEBURTSTAG, MEINE PUPPE</h1>
+      <video src={video} controls autoplay loop className='videoPlayer'></video>
+      <h1 className='clickhere8'>Mein liebes Baby</h1>
     </div>
   )
 }
