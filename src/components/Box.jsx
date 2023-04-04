14a5270ca905682179e72e8bf56ca32b263ab8f2
dipanc1 <dipanchhabra@gmail.com>
2023-04-04 09:05:43 +0530

    pictures and video left

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -2,33 +2,32 @@ import React from 'react'
 import Arrow from './Arrow';
 import Birthday from './Birthday';
 import './box.css'
+import GiftBoxAnimation from './GiftBoxAnimation';
 
 const Box = () => {
     const [card, setCard] = React.useState(false);
-    const handleClick = () => {
-        setCard(true);
-        console.log('card');
-    }
+
     return (
         !card ? (
             <>
-            <h1 className='clickhere'>Click Here!!</h1>
+                <h1 className='clickhere'>Click Here!!</h1>
                 <Arrow />
-                <div className="birthday-gift">
-                    <div className="gift">
-                        <input id='click' type='checkbox' />
-                        <label className='click' htmlFor='click' onClick={handleClick}></label>
-                        <div className="wishes">Happy Birthday!</div>
-                        <div className="sparkles">
+                <GiftBoxAnimation setCard={setCard} />
+                {/* <div className="birthday-gift"> */}
+                {/* <div className="gift"> */}
+                {/* <input id='click' type='checkbox' /> */}
+                {/* <label className='click' htmlFor='click' onClick={handleClick}></label>
+                        <div className="wishes">Happy Birthday!</div> */}
+                {/* <div className="sparkles">
                             <div className="spark1"></div>
                             <div className="spark2"></div>
                             <div className="spark3"></div>
                             <div className="spark4"></div>
                             <div className="spark5"></div>
                             <div className="spark6"></div>
-                        </div>
-                    </div>
-                </div>
+                        </div> */}
+                {/* </div> */}
+                {/* </div> */}
             </>
         ) : (<Birthday />)
     )
---
+++ src/components/GiftBoxAnimation.js
@@ -0,0 +1,64 @@
+import React, { useReducer } from "react";
+import "./giftboxanimation.css";
+
+import box from "./images/box.png";
+import boxLid from "./images/box-lid.png";
+import kuku from "./images/jump-character.png";
+// import ConfettiGenerator from "./CanvasConfetti";
+import Confetti from "./confetti/Confetti";
+
+const init_state = {
+    move: "move",
+    jump: "",
+    rotated: "",
+    rotating: ""
+};
+export default function GiftBoxAnimation({ setCard }) {
+    const [state, setState] = useReducer(
+        (state, new_state) => ({
+            ...state,
+            ...new_state
+        }),
+        init_state
+    );
+
+    const { move, rotating, rotated, jump } = state;
+
+    function animate() {
+        let isDone = rotated === "rotated" ? true : false;
+        setTimeout(() => {
+            setCard(true);
+        }, 6000);
+
+        if (!isDone) {
+            setState({ rotating: "rotating" });
+            setTimeout(() => {
+                setState({ jump: "jump" });
+            }, 300);
+            setTimeout(() => {
+                setState({ rotated: "rotated" });
+            }, 1000);
+        } else {
+            setState(init_state);
+        }
+        let moving = move === "move" ? "" : "move";
+        setState({ move: moving });
+    }
+
+    return (
+        <div className="App">
+            <Confetti open={jump === "jump"} />
+            <div className="img-container">
+                <img className={`kuku ${jump}`} src={kuku} alt="kuku" />
+                <button className="box" onClick={() => animate()}>
+                    <img src={box} alt="box" />
+                </button>
+                <img
+                    className={`lid ${move} ${rotating} ${rotated}`}
+                    src={boxLid}
+                    alt="box-lid"
+                />
+            </div>
+        </div>
+    );
+}
